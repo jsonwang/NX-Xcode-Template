785eb6ae61c075520115961f8f552449151138b8
jsonwang <287971051@qq.com.com>
2017-02-08 11:58:06 +0800

    Xcode 工程模版

---
+++ NXFramework Application.xctemplate/Classes/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  NXFramework
+//
+//  Created by AK on 2017/2/8.
+//  Copyright © 2017年 yoyo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder<UIApplicationDelegate>
+
+@property(strong, nonatomic) UIWindow *window;
+
+@end
+
